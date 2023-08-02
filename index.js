d85d378a26096d23ea60e94bb1bb0e5f6ebba275
samyakjain0113 <86047329+samyakjain0113@users.noreply.github.com>
2023-08-02 20:03:09 +0530

    trial changes

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const path = require('path');
 const app = express();
 
 app.get('/download', (req, res) => {
-  const pdfString = 'This is a dummy PDF string.';
-  const fileName = 'dummy.pdf';
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-  res.send(pdfString);
+
+  const useragent = navigator.userAgent;
+  if(useragent.includes('Instagram')){
+    const pdfString = 'This is a dummy PDF string.';
+    const fileName = 'dummy.pdf';
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+    res.send(pdfString);
+  }else{
+    window.location.href = 'https://experience.4excelerate.org/'
+  }
 });
 
 app.listen(3000, () => {
